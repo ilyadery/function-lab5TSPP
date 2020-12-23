1886b9125123bd70ccc9a82abea56cc618ab5a90
ilyadery <ilyadrbn@gmail.com>
2020-12-23 19:57:23 +0200

    Добавьте файлы проекта.

---
+++ Lab 5 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, x;
+            double y;
+            String Str;
+            Console.Title = "Operator If";
+            Console.Write("Введите значение х: ");
+            Str = Console.ReadLine();
+            x = Convert.ToDouble(Str);
+            Console.Write("Введите значение b: ");
+            Str = Console.ReadLine();
+            b = Convert.ToDouble(Str);
+            Console.Write("Введите значение a: ");
+            Str = Console.ReadLine();
+            a = Convert.ToDouble(Str);
+            Console.Write("Введите значение c: ");
+            Str = Console.ReadLine();
+            c = Convert.ToDouble(Str);
+            if (x < 0 && b != 0)
+            {
+                y = a * (int)Math.Pow(x, 2) + b * x + c;
+            }
+            else if (x > 0 && b == 0)
+            {
+                y = (x - a) / (x - c);
+            }
+            else
+            {
+                y = x / c;
+            }          
+            Console.Write("Результат равен: " + y);
+            Console.WriteLine(Str);
+            Console.Write("Для завершения нажмите Enter");
+            Console.Read();
+        }
+    }
+}
+
+
